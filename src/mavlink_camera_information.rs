de8f943f94a20cfc810866a6132fca80341bcfdb
Patrick José Pereira <patrickelectric@gmail.com>
2020-04-29 15:47:32 -0300

    mavlink_camera_information: Update mavtype and autopilot values
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/mavlink_camera_information.rs
+++ src/mavlink_camera_information.rs
@@ -43,8 +43,8 @@ impl MavlinkCameraInformation {
     fn heartbeat_message() -> mavlink::common::MavMessage {
         mavlink::common::MavMessage::HEARTBEAT(mavlink::common::HEARTBEAT_DATA {
             custom_mode: 0,
-            mavtype: mavlink::common::MavType::MAV_TYPE_QUADROTOR,
-            autopilot: mavlink::common::MavAutopilot::MAV_AUTOPILOT_ARDUPILOTMEGA,
+            mavtype: mavlink::common::MavType::MAV_TYPE_CAMERA,
+            autopilot: mavlink::common::MavAutopilot::MAV_AUTOPILOT_GENERIC,
             base_mode: mavlink::common::MavModeFlag::empty(),
             system_status: mavlink::common::MavState::MAV_STATE_STANDBY,
             mavlink_version: 0x3,
