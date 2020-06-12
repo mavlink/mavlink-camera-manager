f0037fee01113e3e9867a8c6a9e0b7d8a893597b
Patrick José Pereira <patrickelectric@gmail.com>
2020-06-12 17:21:41 -0300

    final version
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/gst/pipeline_runner.rs
+++ src/gst/pipeline_runner.rs
@@ -23,13 +23,10 @@ impl Default for PipelineRunner {
 impl PipelineRunner {
     pub fn set_pipeline(&mut self, pipeline: &str) {
         self.pipeline = String::from(pipeline);
-        *self.stop.lock().unwrap() = false;
     }
 
     pub fn run_loop(&self) {
-        //let self_structure = self.clone();
-        //let closure = move || PipelineRunner::pipeline_loop(self);
-        //gstreamer_runner::run(closure);
+        *self.stop.lock().unwrap() = false;
         PipelineRunner::pipeline_loop(self);
     }
 
@@ -46,63 +43,78 @@ impl PipelineRunner {
             }
         }
 
-        // Create pipeline from string
-        let mut context = gstreamer::ParseContext::new();
-        let pipeline = match gstreamer::parse_launch_full(
-            &pipeline_runner.pipeline,
-            Some(&mut context),
-            gstreamer::ParseFlags::NONE,
-        ) {
-            Ok(pipeline) => Arc::new(pipeline),
-            Err(err) => {
-                if let Some(gstreamer::ParseError::NoSuchElement) =
-                    err.kind::<gstreamer::ParseError>()
-                {
-                    println!(
-                        "Error: Missing element(s): {:?}",
-                        context.get_missing_elements()
-                    );
-                } else {
-                    println!("Error: Failed to parse pipeline: {}", err);
-                }
-                std::process::exit(-1)
-            }
-        };
-
-        let bus = pipeline.get_bus().unwrap();
-
-        let result = pipeline.set_state(gstreamer::State::Playing);
-        if result.is_err() {
-            eprintln!(
-                "Unable to set the pipeline to the `Playing` state (check the bus for error messages)."
-            );
-        }
+        let mut pipeline;
 
         'externalLoop: loop {
-            if *pipeline_runner.stop.lock().unwrap() == true {
-                break;
+
+            // Create pipeline from string
+            let mut context = gstreamer::ParseContext::new();
+            pipeline = match gstreamer::parse_launch_full(
+                &pipeline_runner.pipeline,
+                Some(&mut context),
+                gstreamer::ParseFlags::NONE,
+            ) {
+                Ok(pipeline) => Arc::new(pipeline),
+                Err(err) => {
+                    if let Some(gstreamer::ParseError::NoSuchElement) =
+                        err.kind::<gstreamer::ParseError>()
+                    {
+                        println!(
+                            "Error: Missing element(s): {:?}",
+                            context.get_missing_elements()
+                        );
+                    } else {
+                        println!("Error: Failed to parse pipeline: {}", err);
+                    }
+                    std::process::exit(-1)
+                }
+            };
+
+            let bus = pipeline.get_bus().unwrap();
+
+            let result = pipeline.set_state(gstreamer::State::Playing);
+            if result.is_err() {
+                eprintln!(
+                    "Unable to set the pipeline to the `Playing` state (check the bus for error messages)."
+                );
             }
 
-            for msg in bus.timed_pop(gstreamer::ClockTime::from_mseconds(100)) {
-                use gstreamer::MessageView;
+            // Check if we need to break external loop
+            'internalLoop: loop {
+                if *pipeline_runner.stop.lock().unwrap() == true {
+                    break 'externalLoop;
+                }
 
-                match msg.view() {
-                    MessageView::Eos(..) => {
-                        println!("EOS!");
-                        break 'externalLoop;
-                    },
-                    MessageView::Error(err) => {
-                        print!(
-                            "Error from {:?}: {} ({:?})\n",
-                            err.get_src().map(|s| s.get_path_string()),
-                            err.get_error(),
-                            err.get_debug()
-                        );
-                        break;
+                for msg in bus.timed_pop(gstreamer::ClockTime::from_mseconds(100)) {
+                    use gstreamer::MessageView;
+
+                    match msg.view() {
+                        MessageView::Eos(..) => {
+                            println!("EOS received.");
+                            // Broke internal loop to restart pipeline
+                            break 'internalLoop;
+                        },
+                        MessageView::Error(err) => {
+                            print!(
+                                "Error from {:?}: {} ({:?})\n",
+                                err.get_src().map(|s| s.get_path_string()),
+                                err.get_error(),
+                                err.get_debug()
+                            );
+                            break 'internalLoop;
+                        }
+                        _ => (),
                     }
-                    _ => (),
                 }
             }
+
+            let result = pipeline.set_state(gstreamer::State::Null);
+            if result.is_err() {
+                eprintln!("Unable to set the pipeline to the `Null` state");
+            }
+
+            // The loop will restart, add delay to avoid high cpu usage
+            std::thread::sleep(std::time::Duration::from_millis(500));
         }
 
         let result = pipeline.set_state(gstreamer::State::Null);
