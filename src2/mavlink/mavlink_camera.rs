f6186d11179d55a3fff399824a40d34d072fe2b9
Patrick José Pereira <patrickelectric@gmail.com>
2021-02-11 20:34:41 -0300

    mavlink wip
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src2/mavlink/mavlink_camera.rs
+++ src2/mavlink/mavlink_camera.rs
@@ -17,7 +17,7 @@ pub struct MavlinkCameraInformation {
     component: MavlinkCameraComponent,
     mavlink_connection_string: String,
     video_stream_uri: String,
-    vehicle: Arc<Mutex<Box<dyn mavlink::MavConnection<mavlink::common::MavMessage> + Sync + Send>>>,
+    vehicle: Arc<Box<dyn mavlink::MavConnection<mavlink::common::MavMessage> + Sync + Send>>,
 }
 
 #[derive(PartialEq)]
@@ -32,7 +32,7 @@ pub struct MavlinkCameraHandle {
     mavlink_camera_information: Arc<Mutex<MavlinkCameraInformation>>,
     heartbeat_state: Arc<Mutex<ThreadState>>,
     heartbeat_thread: std::thread::JoinHandle<()>,
-    receive_message_state: Arc<Mutex<ThreadState>>,
+    receive_message_state: Arc<Mutex<ThreadState>>, //TODO: unify states
     receive_message_thread: std::thread::JoinHandle<()>,
 }
 
@@ -58,9 +58,7 @@ impl MavlinkCameraInformation {
             mavlink_connection_string: mavlink_connection_string.into(),
             video_stream_uri:
                 "rtsp://wowzaec2demo.streamlock.net:554/vod/mp4:BigBuckBunny_115k.mov".into(),
-            vehicle: Arc::new(Mutex::new(
-                mavlink::connect(&mavlink_connection_string).unwrap(),
-            )),
+            vehicle: Arc::new(mavlink::connect(&mavlink_connection_string).unwrap()),
         }
     }
 }
@@ -125,12 +123,7 @@ fn heartbeat_loop(
         }
 
         println!("send!");
-        if let Err(error) = vehicle
-            .as_ref()
-            .lock()
-            .unwrap()
-            .send(&header, &heartbeat_message())
-        {
+        if let Err(error) = vehicle.as_ref().send(&header, &heartbeat_message()) {
             eprintln!("Failed to send heartbeat: {:?}", error);
         }
     }
@@ -140,6 +133,80 @@ fn receive_message_loop(
     atomic_thread_state: Arc<Mutex<ThreadState>>,
     mavlink_camera_information: Arc<Mutex<MavlinkCameraInformation>>,
 ) {
+    let mut header = mavlink::MavHeader::default();
+    let mavlink_camera_information = mavlink_camera_information.as_ref().lock().unwrap();
+    header.system_id = mavlink_camera_information.component.system_id;
+    header.component_id = mavlink_camera_information.component.component_id;
+
+    let vehicle = mavlink_camera_information.vehicle.clone();
+    drop(mavlink_camera_information);
+
+    loop {
+        let vehicle = vehicle.as_ref();
+        match vehicle.recv() {
+            Ok((_header, msg)) => {
+                match msg {
+                    // Check if there is any camera information request from gcs
+                    mavlink::common::MavMessage::COMMAND_LONG(command_long) => {
+                        match command_long.command {
+                            mavlink::common::MavCmd::MAV_CMD_REQUEST_CAMERA_INFORMATION => {
+                                println!("Sending camera_information..");
+                                let res = vehicle.send(&header, &camera_information());
+                                if res.is_err() {
+                                    println!("Failed to send camera_information: {:?}", res);
+                                }
+                            }
+                            mavlink::common::MavCmd::MAV_CMD_REQUEST_CAMERA_SETTINGS => {
+                                println!("Sending camera_settings..");
+                                let res = vehicle.send(&header, &camera_settings());
+                                if res.is_err() {
+                                    println!("Failed to send camera_settings: {:?}", res);
+                                }
+                            }
+                            mavlink::common::MavCmd::MAV_CMD_REQUEST_STORAGE_INFORMATION => {
+                                println!("Sending camera_storage_information..");
+                                let res = vehicle.send(&header, &camera_storage_information());
+                                if res.is_err() {
+                                    println!(
+                                        "Failed to send camera_storage_information: {:?}",
+                                        res
+                                    );
+                                }
+                            }
+                            mavlink::common::MavCmd::MAV_CMD_REQUEST_CAMERA_CAPTURE_STATUS => {
+                                println!("Sending camera_capture_status..");
+                                let res = vehicle.send(&header, &camera_capture_status());
+                                if res.is_err() {
+                                    println!("Failed to send camera_capture_status: {:?}", res);
+                                }
+                            }
+                            mavlink::common::MavCmd::MAV_CMD_REQUEST_VIDEO_STREAM_INFORMATION => {
+                                println!("Sending video_stream_information..");
+                                let res = vehicle.send(&header, &video_stream_information());
+                                if res.is_err() {
+                                    println!("Failed to send video_stream_information: {:?}", res);
+                                }
+                            }
+                            _ => {
+                                //TODO: reworking this prints
+                                println!("Ignoring command: {:?}", command_long.command);
+                            }
+                        }
+                    }
+                    // We receive a bunch of heartbeat messages, we can ignore it
+                    mavlink::common::MavMessage::HEARTBEAT(_) => {}
+                    // Any other message that is not a heartbeat or command_long
+                    _ => {
+                        //TODO: reworking this prints
+                        println!("Ignoring: {:?}", msg);
+                    }
+                }
+            }
+            Err(e) => {
+                println!("recv error: {:?}", e);
+            }
+        }
+    }
 }
 
 fn heartbeat_message() -> mavlink::common::MavMessage {
@@ -151,4 +218,101 @@ fn heartbeat_message() -> mavlink::common::MavMessage {
         system_status: mavlink::common::MavState::MAV_STATE_STANDBY,
         mavlink_version: 0x3,
     })
+}
+
+fn camera_information() -> mavlink::common::MavMessage {
+    // Create a fixed size array with the camera name
+    let name_str = String::from("name");
+    let mut name: [u8; 32] = [0; 32];
+    for index in 0..name_str.len() as usize {
+        name[index] = name_str.as_bytes()[index];
+    }
+
+    // Send path to our camera configuration file
+    let uri: Vec<char> = format!("{}", "http://0.0.0.0").chars().collect();
+
+    // Send fake data
+    mavlink::common::MavMessage::CAMERA_INFORMATION(mavlink::common::CAMERA_INFORMATION_DATA {
+        time_boot_ms: 0,
+        firmware_version: 0,
+        focal_length: 0.0,
+        sensor_size_h: 0.0,
+        sensor_size_v: 0.0,
+        flags: mavlink::common::CameraCapFlags::CAMERA_CAP_FLAGS_HAS_VIDEO_STREAM,
+        resolution_h: 0,
+        resolution_v: 0,
+        cam_definition_version: 0,
+        vendor_name: name,
+        model_name: name,
+        lens_id: 0,
+        cam_definition_uri: uri,
+    })
+}
+
+fn camera_settings() -> mavlink::common::MavMessage {
+    //Send fake data
+    mavlink::common::MavMessage::CAMERA_SETTINGS(mavlink::common::CAMERA_SETTINGS_DATA {
+        time_boot_ms: 0,
+        zoomLevel: 0.0,
+        focusLevel: 0.0,
+        mode_id: mavlink::common::CameraMode::CAMERA_MODE_VIDEO,
+    })
+}
+
+fn camera_storage_information() -> mavlink::common::MavMessage {
+    //Send fake data
+    mavlink::common::MavMessage::STORAGE_INFORMATION(mavlink::common::STORAGE_INFORMATION_DATA {
+        time_boot_ms: 0,
+        total_capacity: 102400.0,
+        used_capacity: 0.0,
+        available_capacity: 102400.0,
+        read_speed: 1000.0,
+        write_speed: 1000.0,
+        storage_id: 0,
+        storage_count: 0,
+        status: mavlink::common::StorageStatus::STORAGE_STATUS_READY,
+    })
+}
+
+fn camera_capture_status() -> mavlink::common::MavMessage {
+    //Send fake data
+    mavlink::common::MavMessage::CAMERA_CAPTURE_STATUS(
+        mavlink::common::CAMERA_CAPTURE_STATUS_DATA {
+            time_boot_ms: 0,
+            image_interval: 0.0,
+            recording_time_ms: 0,
+            available_capacity: 10000.0,
+            image_status: 0,
+            video_status: 0,
+            image_count: 0,
+        },
+    )
+}
+
+fn video_stream_information() -> mavlink::common::MavMessage {
+    let name_str = String::from("name");
+    let mut name: [char; 32] = ['\0'; 32];
+    for index in 0..name_str.len() as u32 {
+        name[index as usize] = name_str.as_bytes()[index as usize] as char;
+    }
+
+    let uri: Vec<char> = format!("{}\0", "udp:0.0.0.0:5601").chars().collect();
+
+    //The only important information here is the mavtype and uri variables, everything else is fake
+    mavlink::common::MavMessage::VIDEO_STREAM_INFORMATION(
+        mavlink::common::VIDEO_STREAM_INFORMATION_DATA {
+            framerate: 30.0,
+            bitrate: 1000,
+            flags: mavlink::common::VideoStreamStatusFlags::VIDEO_STREAM_STATUS_FLAGS_RUNNING,
+            resolution_h: 1000,
+            resolution_v: 1000,
+            rotation: 0,
+            hfov: 0,
+            stream_id: 1, // Starts at 1, 0 is for broadcast
+            count: 0,
+            mavtype: mavlink::common::VideoStreamType::VIDEO_STREAM_TYPE_RTSP,
+            name: name,
+            uri: uri,
+        },
+    )
 }
