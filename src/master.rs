596a11a36ca4d1c6ac44ea4d0d1e8b17327e69a5
Patrick José Pereira <patrickelectric@gmail.com>
2021-02-20 11:16:16 -0300

    add missing master
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ src/master.rs
@@ -0,0 +1,17 @@
+use super::video::video_source::*;
+use super::video::video_source_usb::VideoSourceUsb;
+
+pub fn run() {
+    /*
+    let cameras = cameras_available();
+    println!("{:#?}", cameras);
+    //TODO: move to box dyn
+    for camera in cameras {
+        match camera {
+            VideoSourceType::Usb(camera) => {
+                println!("{:#?}", camera.resolutions());
+            }
+            _ => {},
+        }
+    }*/
+}
