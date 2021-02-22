3b9a465053d2596390380cdce08890e2017cd632
Patrick José Pereira <patrickelectric@gmail.com>
2021-02-22 11:21:10 -0300

    stream: Add StreamInformation
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/stream/types.rs
+++ src/stream/types.rs
@@ -1,5 +1,13 @@
 use super::video_stream_udp::VideoStreamUdp;
+use crate::video::types::FrameSize;
+use url::Url;
 
 pub enum StreamType {
     UDP(VideoStreamUdp),
+}
+
+pub struct StreamInformation {
+    pub stream_type: StreamType,
+    pub endpoints: Vec<Url>,
+    pub frame_size: FrameSize,
 }
