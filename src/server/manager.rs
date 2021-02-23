e2f501c21f234e3bb60fa6e9b995d6abeb8bb657
Patrick José Pereira <patrickelectric@gmail.com>
2021-02-23 17:01:30 -0300

    server: Add error message about jsons
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/server/manager.rs
+++ src/server/manager.rs
@@ -1,6 +1,20 @@
 use super::pages;
 
-use actix_web::{rt::System, web, App, HttpServer};
+use actix_web::{
+    error::{ErrorBadRequest, JsonPayloadError},
+    rt::System,
+    web, App, HttpRequest, HttpServer,
+};
+
+use log::*;
+
+fn json_error_handler(error: JsonPayloadError, _: &HttpRequest) -> actix_web::Error {
+    warn!("Problem with json: {}", error.to_string());
+    match error {
+        JsonPayloadError::Overflow => JsonPayloadError::Overflow.into(),
+        _ => ErrorBadRequest(error.to_string()),
+    }
+}
 
 // Start REST API server with the desired address
 pub fn run(server_address: &str) {
@@ -10,6 +24,7 @@ pub fn run(server_address: &str) {
     let _ = System::new("http-server");
     HttpServer::new(|| {
         App::new()
+            .data(web::JsonConfig::default().error_handler(json_error_handler))
             .route("/html/{filename:.*}", web::get().to(pages::root))
             .route("/xml", web::post().to(pages::xml))
             .route("/v4l", web::get().to(pages::v4l))
