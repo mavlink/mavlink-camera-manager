7bc5c11883ebb3b85c20eddac2d4c6e83316fcac
Patrick José Pereira <patrickelectric@gmail.com>
2021-03-04 09:42:58 -0300

    video_stream: Add conflicts_with function for VideoAndStreamInformation
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/video_stream/types.rs
+++ src/video_stream/types.rs
@@ -1,7 +1,10 @@
 use crate::stream::types::StreamInformation;
 use crate::video::types::VideoSourceType;
+use crate::video::video_source::VideoSource;
 
 use serde::{Deserialize, Serialize};
+
+use simple_error::SimpleError;
 
 //TODO: move to stream ?
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
@@ -10,3 +13,41 @@ pub struct VideoAndStreamInformation {
     pub stream_information: StreamInformation,
     pub video_source: VideoSourceType,
 }
+
+impl VideoAndStreamInformation {
+    pub fn conflicts_with(&self, other: &VideoAndStreamInformation) -> Result<(), SimpleError> {
+        if self.name == other.name {
+            return Err(SimpleError::new(format!(
+                "Streams have same name: {}",
+                self.name
+            )));
+        }
+
+        if self.video_source.inner().source_string() == other.video_source.inner().source_string() {
+            return Err(SimpleError::new(format!(
+                "Streams have same source: {}",
+                self.video_source.inner().source_string()
+            )));
+        }
+
+        use std::collections::HashSet;
+        let our_endpoints: HashSet<_> = self.stream_information.endpoints.iter().collect();
+        let other_endpoints: HashSet<_> = other.stream_information.endpoints.iter().collect();
+        let common_endpoints: HashSet<_> = our_endpoints.intersection(&other_endpoints).collect();
+
+        if !common_endpoints.is_empty() {
+            return Err(SimpleError::new(format!(
+                "Stream ({other_name} - {other_source}) has common endpoint with Stream ({our_name} - {our_source}) to already existing stream: {endpoints:#?}",
+                other_name = other.name,
+                other_source = other.video_source.inner().source_string(),
+                our_name = self.name,
+                our_source = self.video_source.inner().source_string(),
+                endpoints = common_endpoints
+            )));
+        }
+
+        return Ok(());
+    }
+}
+
+//TODO: Add tests
