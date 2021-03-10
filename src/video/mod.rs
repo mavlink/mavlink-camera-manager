a97dca92986380c7d603814f7e9566a066c9e1ec
Patrick José Pereira <patrickelectric@gmail.com>
2021-03-10 13:08:49 -0300

    video: Add video_source_gst
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/video/mod.rs
+++ src/video/mod.rs
@@ -2,4 +2,5 @@ pub mod types;
 pub mod video_source;
 pub mod xml;
 
+pub mod video_source_gst;
 pub mod video_source_local;
---
+++ src/video/video_source_gst.rs
@@ -0,0 +1,127 @@
+use super::types::*;
+use super::video_source_local::VideoSourceLocal;
+use super::{
+    video_source,
+    video_source::{VideoSource, VideoSourceAvailable},
+};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub enum VideoSourceGstType {
+    // TODO: local should have a pipeline also
+    Local(VideoSourceLocal),
+    Fake(String),
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct VideoSourceGst {
+    pub name: String,
+    pub source: VideoSourceGstType,
+}
+
+impl VideoSource for VideoSourceGst {
+    fn name(&self) -> &String {
+        return &self.name;
+    }
+
+    fn source_string(&self) -> &str {
+        match &self.source {
+            VideoSourceGstType::Local(local) => &local.source_string(),
+            VideoSourceGstType::Fake(string) => &string,
+        }
+    }
+
+    fn formats(&self) -> Vec<Format> {
+        match &self.source {
+            VideoSourceGstType::Local(local) => local.formats(),
+            VideoSourceGstType::Fake(_) => {
+                let intervals: Vec<FrameInterval> = [60, 30, 24, 16, 10, 5]
+                    .iter()
+                    .map(|&frame_interval| FrameInterval {
+                        denominator: frame_interval,
+                        numerator: 1,
+                    })
+                    .collect();
+
+                let sizes = [
+                    (320, 240),
+                    (640, 480),
+                    (720, 480),
+                    (960, 720),
+                    (1280, 720),
+                    (1280, 1080),
+                    (1440, 1080),
+                    (1920, 1080),
+                ]
+                .iter()
+                .map(|&(width, height)| Size {
+                    width,
+                    height,
+                    intervals: intervals.clone(),
+                })
+                .collect();
+
+                vec![Format {
+                    encode: VideoEncodeType::H264,
+                    sizes,
+                }]
+            }
+        }
+    }
+
+    fn set_control_by_name(&self, control_name: &str, value: i64) -> std::io::Result<()> {
+        Err(std::io::Error::new(
+            std::io::ErrorKind::NotFound,
+            "Source doesn't have controls.",
+        ))
+    }
+
+    fn set_control_by_id(&self, control_id: u64, value: i64) -> std::io::Result<()> {
+        Err(std::io::Error::new(
+            std::io::ErrorKind::NotFound,
+            "Source doesn't have controls.",
+        ))
+    }
+
+    fn control_value_by_name(&self, control_name: &str) -> std::io::Result<i64> {
+        Err(std::io::Error::new(
+            std::io::ErrorKind::NotFound,
+            "Source doesn't have controls.",
+        ))
+    }
+
+    fn control_value_by_id(&self, control_id: u64) -> std::io::Result<i64> {
+        Err(std::io::Error::new(
+            std::io::ErrorKind::NotFound,
+            "Source doesn't have controls.",
+        ))
+    }
+
+    fn controls(&self) -> Vec<Control> {
+        vec![]
+    }
+
+    fn is_valid(&self) -> bool {
+        match &self.source {
+            VideoSourceGstType::Local(local) => local.is_valid(),
+            VideoSourceGstType::Fake(string) => match string.as_str() {
+                // All valid members are from: https://gstreamer.freedesktop.org/documentation/videotestsrc/index.html?gi-language=c#members-2
+                "ball" | "bar" | "black" | "blink" | "blue" | "checkers" | "checkers"
+                | "checkers" | "checkers" | "chroma" | "circular" | "gamut" | "gradient"
+                | "green" | "pinwheel" | "red" | "smpte" | "smpte100" | "smpte75" | "snow"
+                | "solid" | "spokes" | "white" | "zone" => true,
+                _ => false,
+            },
+        }
+    }
+}
+
+impl VideoSourceAvailable for VideoSourceGst {
+    fn cameras_available() -> Vec<VideoSourceType> {
+        vec![VideoSourceType::Gst(VideoSourceGst {
+            name: "Fake source".into(),
+            source: VideoSourceGstType::Fake("ball".into()),
+        })]
+    }
+}
