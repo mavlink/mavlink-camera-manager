c95d7580f967547b5127558a17c6248dc861b419
João Antônio Cardoso <joao.maker@gmail.com>
2022-01-26 17:55:48 -0300

    src: stream: Add RTSP video stream types

--- src/stream/types.rs
+++ src/stream/types.rs
@@ -1,4 +1,5 @@
 use super::stream_backend::StreamBackend;
+use super::video_stream_rtsp::VideoStreamRtsp;
 use super::video_stream_udp::VideoStreamUdp;
 use crate::video::types::CaptureConfiguration;
 use crate::video_stream::types::VideoAndStreamInformation;
@@ -10,18 +11,21 @@ use url::Url;
 #[derive(Debug)]
 pub enum StreamType {
     UDP(VideoStreamUdp),
+    RTSP(VideoStreamRtsp),
 }
 
 impl StreamType {
     pub fn inner(&self) -> &(dyn StreamBackend + '_) {
         match self {
             StreamType::UDP(backend) => backend,
+            StreamType::RTSP(backend) => backend,
         }
     }
 
     pub fn mut_inner(&mut self) -> &mut (dyn StreamBackend + '_) {
         match self {
             StreamType::UDP(backend) => backend,
+            StreamType::RTSP(backend) => backend,
         }
     }
 }
