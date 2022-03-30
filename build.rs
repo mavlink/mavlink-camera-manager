5ff3c1ee1d4f1b4904cbcf5937416634e7e8a0db
João Antônio Cardoso <joao.maker@gmail.com>
2022-03-29 23:00:32 -0300

    build: Encapsulate external file download into a function.

--- build.rs
+++ build.rs
@@ -1,6 +1,14 @@
 extern crate reqwest;
 extern crate vergen;
 
+fn file_download(url: &str, output: &str) {
+    let mut resp = reqwest::blocking::get(url).expect(&format!("Failed to download file: {url}"));
+    let file_path = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join(output);
+    let mut output_file =
+        std::fs::File::create(&file_path).expect(&format!("Failed to create file: {file_path:?}"));
+    std::io::copy(&mut resp, &mut output_file).expect("Failed to copy content.");
+}
+
 fn main() {
     // Configure vergen
     let mut config = vergen::Config::default();
@@ -11,12 +19,8 @@ fn main() {
 
     vergen::vergen(config).expect("Unable to generate the cargo keys!");
 
-    // Download vue file
-    let vue_url = "https://unpkg.com/vue@3.0.5/dist/vue.global.js";
-    let mut resp = reqwest::blocking::get(vue_url)
-        .expect(&format!("Failed to download vue file: {}", &vue_url));
-    let vue_file_path = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("src/html/vue.js");
-    let mut output_file = std::fs::File::create(&vue_file_path)
-        .expect(&format!("Failed to create vue file: {:?}", &vue_file_path));
-    std::io::copy(&mut resp, &mut output_file).expect("Failed to copy content.");
+    file_download(
+        "https://unpkg.com/vue@3.0.5/dist/vue.global.js",
+        "src/html/vue.js",
+    );
 }
