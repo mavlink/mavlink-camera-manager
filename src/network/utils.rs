db9edbd8cd2dee4442e5d720504adae56bfe4b11
João Antônio Cardoso <joao.maker@gmail.com>
2022-07-26 18:48:29 -0300

    src: network: Use the ip from default interface.

--- src/network/utils.rs
+++ src/network/utils.rs
@@ -5,29 +5,25 @@ pub fn get_ipv4_addresses() -> Vec<std::net::Ipv4Addr> {
     // Start with 0.0.0.0
     let mut ips = vec![std::net::Ipv4Addr::UNSPECIFIED];
 
-    for interface in pnet::datalink::interfaces() {
-        debug!("Checking interface {:#?}", interface);
-
-        // We could run inside a docker..
-        // but QGC should be running in an ground station computer
-        if interface.name.contains("docker") {
-            continue;
+    let interfaces = pnet::datalink::interfaces();
+    let interface = match interfaces
+        .iter()
+        .find(|e| e.is_up() && !e.is_loopback() && !e.ips.is_empty() && !e.name.contains("docker"))
+    {
+        Some(interface) => interface,
+        None => {
+            warn!("Error while finding the default interface.");
+            return ips;
         }
+    };
+    debug!("Found default interface: {interface:#?}");
 
-        for interface_ip in interface.ips {
-            match interface_ip {
-                pnet::ipnetwork::IpNetwork::V4(interface_ip) => {
-                    let ip = interface_ip.ip();
-                    if ip.is_loopback() {
-                        continue;
-                    }
-
-                    ips.push(ip);
-                }
-                _ => continue,
-            }
+    interface.ips.iter().for_each(|&ip_network| {
+        if let pnet::ipnetwork::IpNetwork::V4(ipv4_network) = ip_network {
+            ips.push(ipv4_network.ip());
         }
-    }
-    debug!("Valid IPs: {:#?}", ips);
+    });
+    debug!("Valid IPs: {ips:#?}");
+
     return ips;
 }
