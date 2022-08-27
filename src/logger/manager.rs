7a84bb85d4e127e39a5831d4c382393ae808b621
João Antônio Cardoso <joao.maker@gmail.com>
2022-08-27 20:46:12 -0300

    src: logger: Configure tracing logger.

--- src/logger/manager.rs
+++ src/logger/manager.rs
@@ -1,43 +1,52 @@
-use log::*;
-use std::io::Write;
-
 use crate::cli;
-use env_logger::fmt::{Color, Style, StyledValue};
 
-// Change style based on the message log level
-fn colored_level<'a>(style: &'a mut Style, level: Level) -> StyledValue<'a, &'static str> {
-    match level {
-        Level::Trace => style.set_color(Color::Magenta).value("TRACE"),
-        Level::Debug => style.set_color(Color::Blue).value("DEBUG"),
-        Level::Info => style.set_color(Color::Green).value("INFO "),
-        Level::Warn => style.set_color(Color::Yellow).value("WARN "),
-        Level::Error => style.set_color(Color::Red).value("ERROR"),
-    }
-}
+use tracing::{metadata::LevelFilter, *};
+use tracing_log::LogTracer;
+use tracing_subscriber::{fmt, layer::SubscriberExt, EnvFilter, Layer};
 
 // Start logger, should be done inside main
 pub fn init() {
-    if cli::manager::is_verbose() {
-        std::env::set_var("RUST_LOG", "debug");
-    }
+    // Redirect all logs from libs using "Log"
+    LogTracer::init_with_filter(tracing::log::LevelFilter::Trace).expect("Failed to set logger");
 
-    env_logger::Builder::from_env(env_logger::Env::default())
-        .format(|buf, record| {
-            let mut style = buf.style();
-            let level = colored_level(&mut style, record.level());
-            let mut style = buf.style();
-            let message = style.set_bold(true).value(record.args());
-            writeln!(
-                buf,
-                "{} {} {}:{}: {}",
-                level,
-                chrono::Local::now().format("%H:%M:%S.%3f"),
-                record.file().unwrap_or("unknown"),
-                record.line().unwrap_or(0),
-                message,
-            )
-        })
-        .init();
+    // Configure the console log
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+        if cli::manager::is_verbose() {
+            EnvFilter::new(LevelFilter::DEBUG.to_string())
+        } else {
+            EnvFilter::new(LevelFilter::INFO.to_string())
+        }
+    });
+    let console_layer = fmt::Layer::new()
+        .with_writer(std::io::stdout)
+        .with_ansi(true)
+        .with_file(true)
+        .with_line_number(true)
+        .with_span_events(fmt::format::FmtSpan::FULL)
+        .with_target(false)
+        .with_thread_ids(true)
+        .with_thread_names(true)
+        .with_filter(env_filter);
+
+    // Configure file log
+    let dir = cli::manager::log_path();
+    let file_appender = tracing_appender::rolling::daily(dir, "mavlink-camera-manager.log");
+    let file_layer = fmt::Layer::new()
+        .with_writer(file_appender)
+        .with_ansi(false)
+        .with_file(true)
+        .with_line_number(true)
+        .with_span_events(fmt::format::FmtSpan::FULL)
+        .with_target(false)
+        .with_thread_ids(true)
+        .with_thread_names(true)
+        .with_filter(LevelFilter::DEBUG);
+
+    // Configure the default subscriber
+    let subscriber = tracing_subscriber::registry()
+        .with(console_layer)
+        .with(file_layer);
+    tracing::subscriber::set_global_default(subscriber).expect("Unable to set a global subscriber");
 
     info!(
         "{}, version: {}-{}, build date: {}",
