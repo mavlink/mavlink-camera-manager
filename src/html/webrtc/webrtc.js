0746a675c3e58e065e23da0ee0fd86ea80896850
João Antônio Cardoso <joao.maker@gmail.com>
2022-09-16 16:24:55 -0300

    src: html: Update WebRTC front-end

--- src/html/webrtc/webrtc.js
+++ src/html/webrtc/webrtc.js
@@ -9,7 +9,8 @@
 
 window.onload = connect
 
-var server_addr = window.location.origin.slice(window.location.protocol.length + 2).split(':')[0]
+// Set this to override the automatic detection in websocketServerConnect()
+const server_addr = window.location.origin.slice(window.location.protocol.length + 2).split(':')[0]
 const signalling_server_addr = server_addr;
 const signalling_server_port = 6021;
 const rtc_configuration = { iceServers: [] }
@@ -20,13 +21,24 @@ var sessions = {}
 
 /* https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript */
 function getOurId() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     });
 }
 
+function Uint8ToString(u8a) {
+    var CHUNK_SZ = 0x8000;
+    var c = [];
+    for (var i = 0; i < u8a.length; i += CHUNK_SZ) {
+        c.push(String.fromCharCode.apply(null, u8a.subarray(i, i + CHUNK_SZ)));
+    }
+    return c.join("");
+}
+
 function Session(our_id, peer_id, closed_callback) {
+    this.id = null;
     this.peer_connection = null;
     this.ws_conn = null;
     this.peer_id = peer_id;
@@ -35,11 +47,11 @@ function Session(our_id, peer_id, closed_callback) {
     this.data_channel = null;
     this.input = null;
 
-    this.getVideoElement = function () {
+    this.getVideoElement = function() {
         return document.getElementById("stream-" + this.our_id);
     };
 
-    this.resetState = function () {
+    this.resetState = function() {
         if (this.peer_connection) {
             this.peer_connection.close();
             this.peer_connection = null;
@@ -63,12 +75,12 @@ function Session(our_id, peer_id, closed_callback) {
         this.data_channel = null;
     };
 
-    this.handleIncomingError = function (error) {
+    this.handleIncomingError = function(error) {
         this.resetState();
         this.closed_callback(this.our_id);
     };
 
-    this.setStatus = function (text) {
+    this.setStatus = function(text) {
         console.log(text);
         var span = document.getElementById("status-" + this.our_id);
         // Don't set the status if it already contains an error
@@ -76,7 +88,7 @@ function Session(our_id, peer_id, closed_callback) {
             span.textContent = text;
     };
 
-    this.setError = function (text) {
+    this.setError = function(text) {
         console.error(text);
         var span = document.getElementById("status-" + this.our_id);
         span.textContent = text;
@@ -86,23 +98,23 @@ function Session(our_id, peer_id, closed_callback) {
     };
 
     // Local description was set, send it to peer
-    this.onLocalDescription = function (desc) {
+    this.onLocalDescription = function(desc) {
         console.log("Got local description: " + JSON.stringify(desc), this);
         var thiz = this;
         this.peer_connection.setLocalDescription(desc).then(() => {
             this.setStatus("Sending SDP answer");
             var sdp = {
                 'type': 'peer',
-                'peerId': this.peer_id,
+                'sessionId': this.id,
                 'sdp': this.peer_connection.localDescription.toJSON()
             };
             this.ws_conn.send(JSON.stringify(sdp));
-        }).catch(function (e) {
+        }).catch(function(e) {
             thiz.setError(e);
         });
     };
 
-    this.onRemoteDescriptionSet = function () {
+    this.onRemoteDescriptionSet = function() {
         this.setStatus("Remote SDP set");
         this.setStatus("Got SDP offer");
         this.peer_connection.createAnswer()
@@ -110,25 +122,25 @@ function Session(our_id, peer_id, closed_callback) {
     }
 
     // SDP offer received from peer, set remote description and create an answer
-    this.onIncomingSDP = function (sdp) {
+    this.onIncomingSDP = function(sdp) {
         var thiz = this;
         this.peer_connection.setRemoteDescription(sdp)
             .then(this.onRemoteDescriptionSet.bind(this))
-            .catch(function (e) {
+            .catch(function(e) {
                 thiz.setError(e)
             });
     };
 
     // ICE candidate received from peer, add it to the peer connection
-    this.onIncomingICE = function (ice) {
+    this.onIncomingICE = function(ice) {
         var candidate = new RTCIceCandidate(ice);
         var thiz = this;
-        this.peer_connection.addIceCandidate(candidate).catch(function (e) {
+        this.peer_connection.addIceCandidate(candidate).catch(function(e) {
             thiz.setError(e)
         });
     };
 
-    this.onServerMessage = function (event) {
+    this.onServerMessage = function(event) {
         console.log("Received " + event.data);
         try {
             msg = JSON.parse(event.data);
@@ -144,6 +156,9 @@ function Session(our_id, peer_id, closed_callback) {
         if (msg.type == "registered") {
             this.setStatus("Registered with server");
             this.connectPeer();
+        } else if (msg.type == "sessionStarted") {
+            this.setStatus("Registered with server");
+            this.id = msg.sessionId;
         } else if (msg.type == "error") {
             this.handleIncomingError(msg.details);
         } else if (msg.type == "endSession") {
@@ -164,30 +179,31 @@ function Session(our_id, peer_id, closed_callback) {
         }
     };
 
-    this.streamIsPlaying = function (e) {
+    this.streamIsPlaying = function(e) {
         this.setStatus("Streaming");
     };
 
-    this.onServerClose = function (event) {
+    this.onServerClose = function(event) {
         this.resetState();
         this.closed_callback(this.our_id);
     };
 
-    this.onServerError = function (event) {
+    this.onServerError = function(event) {
         this.handleIncomingError('Server error');
     };
 
-    this.websocketServerConnect = function () {
+    this.websocketServerConnect = function() {
         // Clear errors in the status span
         var span = document.getElementById("status-" + this.our_id);
         span.classList.remove('error');
         span.textContent = '';
         console.log("Our ID:", this.our_id);
-        var ws_port = ws_port || '6021';
+        console.log(ws_port)
+        var ws_port = signalling_server_port || '8443';
         if (window.location.protocol.startsWith("file")) {
-            var ws_server = ws_server || server_addr;
+            var ws_server = signalling_server_addr || "127.0.0.1";
         } else if (window.location.protocol.startsWith("http")) {
-            var ws_server = ws_server || window.location.hostname;
+            var ws_server = signalling_server_addr || window.location.hostname;
         } else {
             throw new Error("Don't know how to connect to the signalling server with uri" + window.location);
         }
@@ -196,18 +212,15 @@ function Session(our_id, peer_id, closed_callback) {
         this.ws_conn = new WebSocket(ws_url);
         /* When connected, immediately register with the server */
         this.ws_conn.addEventListener('open', (event) => {
-            this.ws_conn.send(JSON.stringify({
-                "type": "register",
-                "peerType": "consumer"
-            }));
-            this.setStatus("Registering with server");
+            this.setStatus("Connecting to the peer");
+            this.connectPeer();
         });
         this.ws_conn.addEventListener('error', this.onServerError.bind(this));
         this.ws_conn.addEventListener('message', this.onServerMessage.bind(this));
         this.ws_conn.addEventListener('close', this.onServerClose.bind(this));
     };
 
-    this.connectPeer = function () {
+    this.connectPeer = function() {
         this.setStatus("Connecting " + this.peer_id);
 
         this.ws_conn.send(JSON.stringify({
@@ -216,7 +229,7 @@ function Session(our_id, peer_id, closed_callback) {
         }));
     };
 
-    this.onRemoteStreamAdded = function (event) {
+    this.onRemoteStreamAdded = function(event) {
         var videoTracks = event.stream.getVideoTracks();
         var audioTracks = event.stream.getAudioTracks();
 
@@ -231,7 +244,7 @@ function Session(our_id, peer_id, closed_callback) {
         }
     };
 
-    this.createCall = function (msg) {
+    this.createCall = function(msg) {
         console.log('Creating RTCPeerConnection');
 
         this.peer_connection = new RTCPeerConnection(rtc_configuration);
@@ -246,17 +259,17 @@ function Session(our_id, peer_id, closed_callback) {
                 this.input = new Input(video_element, (data) => {
                     if (this.data_channel) {
                         console.log(`Navigation data: ${data}`);
-                        this.data_channel.send(data);
+                        this.data_channel.send(JSON.stringify(data));
                     }
                 });
             }
 
             this.data_channel.onopen = (event) => {
-                console.log("Receive channel opened, attaching input");
+                console.log("Receive channel opened, attaching input", event);
                 this.input.attach();
             }
             this.data_channel.onclose = (event) => {
-                console.info("Receive channel closed");
+                console.info("Receive channel closed", event);
                 this.input && this.input.detach();
                 this.data_channel = null;
             }
@@ -295,7 +308,7 @@ function Session(our_id, peer_id, closed_callback) {
             }
             this.ws_conn.send(JSON.stringify({
                 "type": "peer",
-                "peerId": this.peer_id,
+                "sessionId": this.id,
                 "ice": event.candidate.toJSON()
             }));
         };
@@ -308,22 +321,39 @@ function Session(our_id, peer_id, closed_callback) {
     this.websocketServerConnect();
 }
 
+function startSession() {
+    var peer_id = document.getElementById("camera-id").value;
+
+    if (peer_id === "") {
+        return;
+    }
+
+    sessions[peer_id] = new Session(peer_id);
+}
+
 function session_closed(peer_id) {
     sessions[peer_id] = null;
 }
 
-function addPeer(peer_id, display_name) {
-    console.log("Display name: ", display_name);
+function addPeer(peer_id, meta) {
+    console.log("Meta: ", JSON.stringify(meta));
 
-    if (display_name == null) {
-        var li_str = '<li id="peer-' + peer_id + '"><button class="button button1">' + peer_id + '</button></li>';
-    } else {
-        var li_str = '<li id="peer-' + peer_id + '"><button class="button button1">' + display_name + '</button></li>';
+    var nav_ul = document.getElementById("camera-list");
+
+    meta = meta ? meta : { "display-name": peer_id };
+    let display_html = `${meta["display-name"] ? meta["display-name"] : peer_id}<ul>`;
+    for (const key in meta) {
+        if (key != "display-name") {
+            display_html += `<li>- ${key}: ${meta[key]}</li>`;
+        }
     }
+    display_html += "</ul>"
+
+    var li_str = '<li id="peer-' + peer_id + '"><button class="button button1">' + display_html + '</button></li>';
 
     nav_ul.insertAdjacentHTML('beforeend', li_str);
     var li = document.getElementById("peer-" + peer_id);
-    li.onclick = function (e) {
+    li.onclick = function(e) {
         var sessions_div = document.getElementById('sessions');
         var our_id = getOurId();
         var session_div_str = '<div class="session" id="session-' + our_id + '"><video preload="none" class="stream" id="stream-' + our_id + '"></video><p>Status: <span id="status-' + our_id + '">unknown</span></p></div>'
@@ -333,17 +363,11 @@ function addPeer(peer_id, display_name) {
 }
 
 function clearPeers() {
+    var nav_ul = document.getElementById("camera-list");
+
     while (nav_ul.firstChild) {
         nav_ul.removeChild(nav_ul.firstChild);
     }
-}
-
-function clearConnection() {
-    ws_conn.removeEventListener('open', onServerOpen);
-    ws_conn.removeEventListener('error', onServerError);
-    ws_conn.removeEventListener('message', onServerMessage);
-    ws_conn.removeEventListener('close', onServerClose);
-    ws_conn = null;
 }
 
 function onServerMessage(event) {
@@ -353,36 +377,49 @@ function onServerMessage(event) {
         msg = JSON.parse(event.data);
     } catch (e) {
         if (e instanceof SyntaxError) {
-            this.handleIncomingError("Error parsing incoming JSON: " + event.data);
+            console.error("Error parsing incoming JSON: " + event.data);
         } else {
-            this.handleIncomingError("Unknown error parsing response: " + event.data);
+            console.error("Unknown error parsing response: " + event.data);
         }
         return;
     }
 
-    if (msg.type == "registered") {
+    if (msg.type == "welcome") {
+        console.info(`Got welcomed with ID ${msg.peer_id}`);
         ws_conn.send(JSON.stringify({
             "type": "list"
         }));
     } else if (msg.type == "list") {
         clearPeers();
         for (i = 0; i < msg.producers.length; i++) {
-            addPeer(msg.producers[i].id, msg.producers[i].displayName);
+            addPeer(msg.producers[i].id, msg.producers[i].meta);
         }
-    } else if (msg.type == "producerAdded") {
-        addPeer(msg.peerId, msg.displayName);
-    } else if (msg.type == "producerRemoved") {
+    } else if (msg.type == "peerStatusChanged") {
         var li = document.getElementById("peer-" + msg.peerId);
-        li.parentNode.removeChild(li);
+        if (msg.roles.includes("producer")) {
+            if (li == null) {
+                console.error('Adding peer');
+                addPeer(msg.peerId, msg.meta);
+            }
+        } else if (li != null) {
+            li.parentNode.removeChild(li);
+        }
     } else {
-        this.handleIncomingError("Unsupported message: ", msg);
+        console.error("Unsupported message: ", msg);
     }
 };
+
+function clearConnection() {
+    ws_conn.removeEventListener('error', onServerError);
+    ws_conn.removeEventListener('message', onServerMessage);
+    ws_conn.removeEventListener('close', onServerClose);
+    ws_conn = null;
+}
 
 function onServerClose(event) {
     clearConnection();
     clearPeers();
-    console.log("Close");
+    console.log("Close", event);
     window.setTimeout(connect, 1000);
 };
 
@@ -393,19 +430,30 @@ function onServerError(event) {
     window.setTimeout(connect, 1000);
 };
 
-function onServerOpen(event) {
-    ws_conn.send(JSON.stringify({
-        "type": "register",
-        "peerType": "listener"
-    }))
-}
-
 function connect() {
-    var signalling_url = `ws://${signalling_server_addr}:${signalling_server_port}`
-    console.log(`Connecting listener to ${signalling_url}`);
-    ws_conn = new WebSocket(signalling_url);
-    ws_conn.addEventListener('open', onServerOpen);
+    var ws_port = ws_port || '6021';
+    if (window.location.protocol.startsWith("file")) {
+        var ws_server = ws_server || "127.0.0.1";
+    } else if (window.location.protocol.startsWith("http")) {
+        var ws_server = ws_server || window.location.hostname;
+    } else {
+        throw new Error("Don't know how to connect to the signalling server with uri" + window.location);
+    }
+    var ws_url = 'ws://' + ws_server + ':' + ws_port
+    console.log("Connecting listener");
+    ws_conn = new WebSocket(ws_url);
+    ws_conn.addEventListener('open', (event) => {
+        console.log("Open", event)
+        ws_conn.send(JSON.stringify({
+            "type": "setPeerStatus",
+            "roles": ["listener"]
+        }));
+    });
     ws_conn.addEventListener('error', onServerError);
     ws_conn.addEventListener('message', onServerMessage);
     ws_conn.addEventListener('close', onServerClose);
+}
+
+function setup() {
+    connect();
 }
