abdd4f9725a77de05a6df7bd9a87b4dffe26b039
João Antônio Cardoso <joao.maker@gmail.com>
2022-12-14 19:14:24 -0300

    src: stream: pipeline: Add support for JPEG and YUYV encodes

--- src/stream/pipeline/fake_pipeline.rs
+++ src/stream/pipeline/fake_pipeline.rs
@@ -55,7 +55,7 @@ impl FakePipeline {
         // Fakes (videotestsrc) are only "video/x-raw" or "video/x-bayer",
         // and to be able to encode it, we need to define an available
         // format for both its src the next element's sink pad.
-        // We are choosing "UYVY" because it is compatible by the
+        // We are choosing "UYVY" because it is compatible with the
         // application-rtp template capabilities.
         // For more information: https://gstreamer.freedesktop.org/documentation/additional/design/mediatype-video-raw.html?gi-language=c#formats
         let description = match &configuration.encode {
@@ -63,13 +63,55 @@ impl FakePipeline {
                 format!(concat!(
                         "videotestsrc pattern={pattern} is-live=true do-timestamp=true",
                         " ! timeoverlay",
-                        " ! videoconvert",
                         " ! video/x-raw,format=I420",
                         " ! x264enc tune=zerolatency speed-preset=ultrafast bitrate=5000",
                         " ! h264parse",
-                        " ! capsfilter name={filter_name} caps=video/x-h264,profile=constrained-baseline,stream-format=avc,alignment=au,width={width},height={height},framerate={interval_denominator}/{interval_numerator}",
+                        " ! capsfilter name={filter_name} caps=video/x-h264,profile={profile},stream-format=avc,alignment=au,width={width},height={height},framerate={interval_denominator}/{interval_numerator}",
                         " ! rtph264pay aggregate-mode=zero-latency config-interval=10 pt=96",
                         " ! tee name={tee_name} allow-not-linked=true"
+                    ),
+                    pattern = pattern,
+                    profile = "constrained-baseline",
+                    width = configuration.width,
+                    height = configuration.height,
+                    interval_denominator = configuration.frame_interval.denominator,
+                    interval_numerator = configuration.frame_interval.numerator,
+                    filter_name = PIPELINE_FILTER_NAME,
+                    tee_name = PIPELINE_TEE_NAME
+                )
+            }
+            VideoEncodeType::YUYV => {
+                format!(
+                    concat!(
+                        // Because application-rtp templates doesn't accept "YUY2", we
+                        // need to transcode it. We are arbitrarily chosing the closest
+                        // format available ("UYVY").
+                        "videotestsrc pattern={pattern} is-live=true do-timestamp=true",
+                        " ! timeoverlay",
+                        " ! video/x-raw,format=I420",
+                        " ! capsfilter name={filter_name} caps=video/x-raw,format=I420,width={width},height={height},framerate={interval_denominator}/{interval_numerator}",
+                        " ! rtpvrawpay pt=96",
+                        " ! tee name={tee_name} allow-not-linked=true",
+                    ),
+                    pattern = pattern,
+                    width = configuration.width,
+                    height = configuration.height,
+                    interval_denominator = configuration.frame_interval.denominator,
+                    interval_numerator = configuration.frame_interval.numerator,
+                    filter_name = PIPELINE_FILTER_NAME,
+                    tee_name = PIPELINE_TEE_NAME
+                )
+            }
+            VideoEncodeType::MJPG => {
+                format!(
+                    concat!(
+                        "videotestsrc pattern={pattern} is-live=true do-timestamp=true",
+                        " ! timeoverlay",
+                        " ! video/x-raw,format=I420",
+                        " ! jpegenc quality=85 idct-method=1",
+                        " ! capsfilter name={filter_name} caps=image/jpeg,width={width},height={height},framerate={interval_denominator}/{interval_numerator}",
+                        " ! rtpjpegpay pt=96",
+                        " ! tee name={tee_name} allow-not-linked=true",
                     ),
                     pattern = pattern,
                     width = configuration.width,
@@ -82,7 +124,7 @@ impl FakePipeline {
             }
             unsupported => {
                 return Err(anyhow!(
-                    "Encode {unsupported:?} is not supported for V4l Pipeline"
+                    "Encode {unsupported:?} is not supported for Test Pipeline"
                 ))
             }
         };
