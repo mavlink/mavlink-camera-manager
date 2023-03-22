0b59211d55a6757d8f94ae13d131752dc4bf648e
João Antônio Cardoso <joao.maker@gmail.com>
2023-03-22 09:14:03 -0300

    src: video: Remove h264 profile identification
    This was a workaround and is no longer needed since we fixed the
    initialization of the Pipelines and Sinks

--- src/stream/pipeline/v4l_pipeline.rs
+++ src/stream/pipeline/v4l_pipeline.rs
@@ -1,9 +1,6 @@
 use crate::{
-    stream::{gst::utils::wait_for_element_state, types::CaptureConfiguration},
-    video::{
-        types::{VideoEncodeType, VideoSourceType},
-        video_source_local::find_h264_profile_for_device,
-    },
+    stream::types::CaptureConfiguration,
+    video::types::{VideoEncodeType, VideoSourceType},
     video_stream::types::VideoAndStreamInformation,
 };
 
@@ -11,7 +8,7 @@ use super::{
     PipelineGstreamerInterface, PipelineState, PIPELINE_FILTER_NAME, PIPELINE_SINK_TEE_NAME,
 };
 
-use anyhow::{anyhow, Context, Result};
+use anyhow::{anyhow, Result};
 
 use tracing::*;
 
@@ -53,21 +50,15 @@ impl V4lPipeline {
 
         let description = match &configuration.encode {
             VideoEncodeType::H264 => {
-                let Some(profile) = find_h264_profile_for_device(device, &width, &height, &interval_numerator, &interval_denominator) else {
-                    return Err(anyhow!(
-                        "The device {device:#?} doesn't support any of our known H264 profiles"
-                    ));
-                };
                 format!(
                     concat!(
                         "v4l2src device={device} do-timestamp=false",
                         " ! h264parse",
-                        " ! capsfilter name={filter_name} caps=video/x-h264,stream-format=avc,alignment=au,profile={profile},width={width},height={height},framerate={interval_denominator}/{interval_numerator}",
+                        " ! capsfilter name={filter_name} caps=video/x-h264,stream-format=avc,alignment=au,width={width},height={height},framerate={interval_denominator}/{interval_numerator}",
                         " ! rtph264pay aggregate-mode=zero-latency config-interval=10 pt=96",
                         " ! tee name={sink_tee_name} allow-not-linked=true"
                     ),
                     device = device,
-                    profile = profile,
                     width = width,
                     height = height,
                     interval_denominator = interval_denominator,
@@ -136,94 +127,4 @@ impl PipelineGstreamerInterface for V4lPipeline {
     fn is_running(&self) -> bool {
         self.state.pipeline_runner.is_running()
     }
-}
-
-#[instrument(level = "debug")]
-pub fn get_default_v4l2_h264_profile(
-    device: &str,
-    width: &u32,
-    height: &u32,
-    interval_numerator: &u32,
-    interval_denominator: &u32,
-) -> Result<String> {
-    // Here we are getting the profile chosen by gstreamer because if we don't set it in the capsfilter
-    // before it is set to playing, when the second Sink connects to it, the pipeline will try to
-    // renegotiate this capability, freezing the already-playing sinks for a while.
-
-    if let Err(error) = gst::init() {
-        error!("Error! {error}");
-    };
-
-    let description = format!(
-        concat!(
-            "v4l2src device={device} do-timestamp=false num-buffers=1",
-            " ! h264parse ",
-            " ! capsfilter name=Filter caps=video/x-h264,stream-format=avc,alignment=au,width={width},height={height},framerate={interval_numerator}/{interval_denominator}",
-            " ! fakesink",
-        ),
-        device = device,
-        width = width,
-        height = height,
-        interval_denominator = interval_denominator,
-        interval_numerator = interval_numerator,
-    );
-
-    let pipeline = gst::parse_launch(&description)?
-        .downcast::<gst::Pipeline>()
-        .expect("Couldn't downcast pipeline");
-
-    debug!("Starting...");
-    pipeline
-        .set_state(gst::State::Playing)
-        .expect("Unable to set the pipeline to the `Playing` state");
-    if let Err(error) = wait_for_element_state(
-        pipeline.upcast_ref::<gst::Element>(),
-        gst::State::Playing,
-        100,
-        2,
-    ) {
-        let _ = pipeline.set_state(gst::State::Null);
-        return Err(anyhow!(
-            "Failed setting Pipeline state to Playing to get H264 Profile. Reason: {error:?}"
-        ));
-    }
-
-    debug!("Getting current profile...");
-    let profile = pipeline
-        .by_name("Filter")
-        .context("Failed to access/find capsfilter element named \"Filter\"")?
-        .static_pad("src")
-        .context("Failed to access static src pad from capsfilter")?
-        .current_caps()
-        .context("capsfilter with a srcpad without any caps")?
-        .iter()
-        .find_map(|structure| {
-            structure.iter().find_map(|(key, sendvalue)| {
-                if key == "profile" {
-                    Some(sendvalue.to_value().get::<String>().unwrap())
-                } else {
-                    None
-                }
-            })
-        })
-        .context(
-            "Coudn't find any field called \"profile\" in srcpad caps of the capsfilter element",
-        )?;
-
-    debug!("Finishing...");
-    pipeline
-        .set_state(gst::State::Null)
-        .context("Unable to set the pipeline to the `Null` state")?;
-    if let Err(error) = wait_for_element_state(
-        pipeline.upcast_ref::<gst::Element>(),
-        gst::State::Null,
-        100,
-        2,
-    ) {
-        return Err(anyhow!(
-            "Failed setting Pipeline to Null state. Reason: {error:?}"
-        ));
-    }
-
-    Ok(profile)
 }
