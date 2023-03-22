8223d6ef782f4ef0737a11e0366e4305c46f0b02
João Antônio Cardoso <joao.maker@gmail.com>
2023-03-22 16:32:42 -0300

    src: main: Deal with the new return type of start_default()

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate paperclip;
 extern crate sys_info;
 extern crate tracing;
 
+use tracing::*;
 mod cli;
 mod custom;
 mod logger;
@@ -31,7 +32,9 @@ async fn main() -> Result<(), std::io::Error> {
 
     stream::webrtc::signalling_server::SignallingServer::default();
 
-    stream::manager::start_default();
+    if let Err(error) = stream::manager::start_default() {
+        error!("Failed to start default streams. Reason: {error:?}")
+    }
 
     server::manager::run(cli::manager::server_address()).await
 }
