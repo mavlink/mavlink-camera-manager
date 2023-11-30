978f586f6a7a926b3fc2b3311fe62da228b3362a
Patrick José Pereira <patrickelectric@gmail.com>
2023-11-30 09:38:51 -0300

    helper: threads: Add process_task_counter function
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/helper/threads.rs
+++ src/helper/threads.rs
@@ -1,17 +1,20 @@
+use cached::proc_macro::cached;
 use std::thread;
 use std::time::Duration;
 use sysinfo::{System, SystemExt};
 use tracing::*;
 
-pub fn start_thread_counter_thread() {
+#[cached(time = 1)]
+pub fn process_task_counter() -> usize {
     let mut system = System::new_all();
     let pid = sysinfo::get_current_pid().expect("Failed to get current PID.");
+    system.refresh_process(pid);
+    system.process(pid).unwrap().tasks.len()
+}
+
+pub fn start_thread_counter_thread() {
     thread::spawn(move || loop {
-        system.refresh_process(pid);
-        info!(
-            "Number of child processes: {}",
-            system.process(pid).unwrap().tasks.len()
-        );
+        info!("Number of child processes: {}", process_task_counter());
         thread::sleep(Duration::from_secs(1));
     });
 }
