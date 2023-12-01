6a1ef6923fba69b9ecf3fdaedd1e685e2011ec31
Patrick José Pereira <patrickelectric@gmail.com>
2023-12-01 15:01:20 -0300

    custom: Add WebRTCTest
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/custom/mod.rs
+++ src/custom/mod.rs
@@ -4,18 +4,21 @@ use crate::cli;
 use crate::video_stream::types::VideoAndStreamInformation;
 
 mod bluerov;
+mod test;
 
 #[derive(ValueEnum, PartialEq, Debug, Clone)]
 #[clap(rename_all = "verbatim")]
 pub enum CustomEnvironment {
     BlueROVUDP,
     BlueROVRTSP,
+    WebRTCTest,
 }
 
 pub fn create_default_streams() -> Vec<VideoAndStreamInformation> {
     match cli::manager::default_settings() {
         Some(CustomEnvironment::BlueROVUDP) => bluerov::udp(),
         Some(CustomEnvironment::BlueROVRTSP) => bluerov::rtsp(),
+        Some(CustomEnvironment::WebRTCTest) => test::take_webrtc_stream(),
         _ => vec![],
     }
 }
