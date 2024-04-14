457ebabb04eb5f5fa2eeeb9323fa40eca961548d
João Antônio Cardoso <joao.maker@gmail.com>
2024-04-14 17:32:57 -0300

    src: helper: Improve WebRTC Test framework

--- src/helper/develop.rs
+++ src/helper/develop.rs
@@ -8,10 +8,15 @@ use tokio::runtime::Runtime;
 use tracing::*;
 
 async fn task(mut counter: i32) -> Result<()> {
-    info!("Started webrtc test..");
+    // Wait for the system to stabilize
+    tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+
+    let initial_number_of_tasks = helper::threads::process_task_counter();
+
+    info!("Started webrtc test. Number of tasks: {initial_number_of_tasks}");
 
     let mut caps = DesiredCapabilities::chrome();
-    let _ = caps.set_headless();
+    caps.set_headless()?;
 
     let port = cli::manager::enable_webrtc_task_test().unwrap();
     let driver = WebDriver::new(&format!("http://localhost:{}", port), caps)
@@ -26,24 +31,52 @@ async fn task(mut counter: i32) -> Result<()> {
         .await
         .expect("Failed to connect to local webrtc page.");
 
+    let mut number_of_tasks_last_cycle = initial_number_of_tasks;
     loop {
-        for button in ["add-consumer", "add-session", "remove-all-consumers"] {
-            info!("Looking for element: {button}");
-            driver
-                .query(By::Id(button))
-                .wait(Duration::from_secs(60), Duration::from_millis(100))
+        for button_id in [
+            "add-consumer",
+            "add-session",
+            "remove-session",
+            "remove-consumer",
+        ] {
+            info!("Looking for html element id: {button_id:?}");
+
+            let button = driver
+                .query(By::Id(button_id))
+                .and_enabled()
+                .and_clickable()
+                .and_displayed()
+                .wait(Duration::from_secs(1), Duration::from_millis(100))
                 .first()
-                .await?
-                .click()
-                .await?;
+                .await
+                .inspect_err(|error| {
+                    error!("Failed to query button id {button_id:?}: {error:?}")
+                })?;
+
+            button.click().await.inspect_err(|error| {
+                error!("Failed to click button id {button_id:?}: {error:?}")
+            })?;
+
+            info!("Button id {button_id:?}: clicked");
+
+            tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
         }
 
         counter += 1;
 
-        info!("Restarted webrtc {} times", counter);
         let number_of_tasks = helper::threads::process_task_counter();
-        if number_of_tasks > 100 {
-            error!("Thead leak detected: {number_of_tasks}");
+        let number_of_new_tasks_since_start =
+            number_of_tasks as i32 - initial_number_of_tasks as i32;
+        let number_of_new_tasks_since_last_cycle =
+            number_of_tasks as i32 - number_of_tasks_last_cycle as i32;
+        number_of_tasks_last_cycle = number_of_tasks;
+
+        info!("Recycled webrtc {counter} times");
+        info!("New tasks since start: {number_of_new_tasks_since_start}");
+        info!("New tasks since last cycle: {number_of_new_tasks_since_last_cycle}");
+
+        if counter > 1 && number_of_new_tasks_since_last_cycle > 1 {
+            error!("Thead leak detected!");
             std::process::exit(-1);
         }
     }
