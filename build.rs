6f4240359d982d26708e5f94feb13562d550998d
Patrick José Pereira <patrickelectric@gmail.com>
2024-07-15 17:14:35 -0300

    build: Add link for pnet dependency
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- build.rs
+++ build.rs
@@ -21,7 +21,24 @@ fn file_download(url: &str, output: &str) {
     std::io::copy(&mut resp, &mut output_file).expect("Failed to copy content.");
 }
 
+#[cfg(windows)]
+fn print_link_search_path() {
+    use std::env;
+
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    if cfg!(target_arch = "x86_64") {
+        println!("cargo:rustc-link-search=native={}/lib/x64", manifest_dir);
+    } else {
+        println!("cargo:rustc-link-search=native={}/lib", manifest_dir);
+    }
+}
+
+#[cfg(not(windows))]
+fn print_link_search_path() {}
+
 fn main() {
+    print_link_search_path();
+
     // Configure vergen
     let mut config = vergen::Config::default();
     *config.build_mut().semver_mut() = true;
