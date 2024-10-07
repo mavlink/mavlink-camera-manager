90b0eca85a15c053746a91ed78ceed0fe2d2b2d4
João Antônio Cardoso <joao.maker@gmail.com>
2024-10-07 18:57:23 -0300

    src: libn: stream: sink: Enable Nack

--- src/lib/stream/sink/webrtc_sink.rs
+++ src/lib/stream/sink/webrtc_sink.rs
@@ -44,7 +44,7 @@ impl SinkInterface for WebRTCSink {
             "direction",
             gst_webrtc::WebRTCRTPTransceiverDirection::Sendonly,
         );
-        transceiver.set_property("do-nack", false);
+        transceiver.set_property("do-nack", true);
         transceiver.set_property("fec-type", gst_webrtc::WebRTCFECType::None);
 
         // Link
