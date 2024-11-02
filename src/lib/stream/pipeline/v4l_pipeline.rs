e416d7772bfbfd465b3e86fd5921e353dbcd5a03
João Antônio Cardoso <joao.maker@gmail.com>
2024-11-02 18:40:18 -0300

    src: lib: stream: sink: Fix typo

--- src/lib/stream/pipeline/v4l_pipeline.rs
+++ src/lib/stream/pipeline/v4l_pipeline.rs
@@ -57,7 +57,7 @@ impl V4lPipeline {
                 format!(
                     concat!(
                         "v4l2src device={device} do-timestamp=true",
-                        " ! h264parse",  // Here we need the parse to help the stream-format and alignment part, which is being fixed here because avc/au seems to reduce the CPU usage in the RTP payloading part.
+                        " ! h264parse",  // Here we need the parse to help the stream-format and alignment part, which is being fixated here because avc/au seems to reduce the CPU usage in the RTP payloading part.
                         " ! capsfilter name={filter_name} caps=video/x-h264,stream-format=avc,alignment=au,width={width},height={height},framerate={interval_denominator}/{interval_numerator}",
                         " ! tee name={video_tee_name} allow-not-linked=true",
                         " ! rtph264pay aggregate-mode=zero-latency config-interval=10 pt=96",
