e434dbbb883c5981e43e827b66a17f6b6c45b063
Patrick José Pereira <patrickelectric@gmail.com>
2025-06-25 17:46:28 -0300

    lib: streams: sink: zenoh: Use CDR over json
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ src/lib/stream/sink/types/mod.rs
@@ -0,0 +1,1 @@
+pub mod zenoh_message;
---
+++ src/lib/stream/sink/types/zenoh_message.rs
@@ -0,0 +1,67 @@
+use serde::Serialize;
+
+/// A timestamp, represented as an offset from a user-defined epoch.
+/// <https://docs.foxglove.dev/docs/visualization/message-schemas/built-in-types#time>
+#[derive(Debug, Serialize)]
+pub struct Timestamp {
+    /// Seconds since epoch.
+    sec: u32,
+    /// Additional nanoseconds since epoch.
+    nsec: u32,
+}
+
+impl Timestamp {
+    pub fn new(sec: u32, nsec: u32) -> Self {
+        Self { sec, nsec }
+    }
+
+    pub fn now() -> Self {
+        let now = chrono::Utc::now();
+        Self {
+            sec: now.timestamp() as u32,
+            nsec: now.timestamp_subsec_nanos() as u32,
+        }
+    }
+}
+
+/// A single frame of a compressed video bitstream
+/// <https://docs.foxglove.dev/docs/visualization/message-schemas/compressed-video>
+#[derive(Debug, Serialize)]
+pub struct CompressedVideo {
+    /// Timestamp of video frame
+    pub timestamp: Timestamp,
+    /// Frame of reference for the video.
+    ///
+    /// The origin of the frame is the optical center of the camera. +x points to the right in the video, +y points down, and +z points into the plane of the video.
+    pub frame_id: String,
+    /// Compressed video frame data.
+    ///
+    /// For packet-based video codecs this data must begin and end on packet boundaries (no partial packets), and must contain enough video packets to decode exactly one image (either a keyframe or delta frame). Note: Foxglove does not support video streams that include B frames because they require lookahead.
+    ///
+    /// Specifically, the requirements for different `format` values are:
+    ///
+    /// - `h264`
+    ///    - Use Annex B formatted data
+    ///    - Each CompressedVideo message should contain enough NAL units to decode exactly one video frame
+    ///    - Each message containing a key frame (IDR) must also include a SPS NAL unit
+    ///
+    /// - `h265` (HEVC)
+    ///    - Use Annex B formatted data
+    ///    - Each CompressedVideo message should contain enough NAL units to decode exactly one video frame
+    ///    - Each message containing a key frame (IRAP) must also include relevant VPS/SPS/PPS NAL units
+    ///
+    /// - `vp9`
+    ///    - Each CompressedVideo message should contain exactly one video frame
+    ///
+    /// - `av1`
+    ///    - Use the "Low overhead bitstream format" (section 5.2)
+    ///    - Each CompressedVideo message should contain enough OBUs to decode exactly one video frame
+    ///    - Each message containing a key frame must also include a Sequence Header OBU
+    pub data: Vec<u8>,
+    /// Video format.
+    ///
+    /// Supported values: `h264`, `h265`, `vp9`, `av1`.
+    ///
+    /// Note: compressed video support is subject to hardware limitations and patent licensing, so not all encodings may be supported on all platforms. See more about [H.265 support](<https://caniuse.com/hevc>), [VP9 support](<https://caniuse.com/webm>), and [AV1 support](<https://caniuse.com/av1>).
+    pub format: String,
+}
