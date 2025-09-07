0a7fc35225c8b224537c506fd0987cd33a9decab
Patrick José Pereira <patrickelectric@gmail.com>
2025-09-07 09:13:15 +0100

    video: Add macos support
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/lib/video/local/mod.rs
+++ src/lib/video/local/mod.rs
@@ -1,4 +1,6 @@
 #[cfg(target_os = "linux")]
 pub mod video_source_local_linux;
-#[cfg(not(target_os = "linux"))]
+#[cfg(target_os = "macos")]
+pub mod video_source_local_macos;
+#[cfg(not(any(target_os = "linux", target_os = "macos")))]
 pub mod video_source_local_none;
--- src/lib/video/video_source_local.rs
+++ src/lib/video/video_source_local.rs
@@ -1,6 +1,8 @@
 #[cfg(target_os = "linux")]
 use super::local::video_source_local_linux as source;
-#[cfg(not(target_os = "linux"))]
+#[cfg(target_os = "macos")]
+use super::local::video_source_local_macos as source;
+#[cfg(not(any(target_os = "linux", target_os = "macos")))]
 use super::local::video_source_local_none as source;
 
 pub type VideoSourceLocal = source::VideoSourceLocal;
