066244f309ba2753035f3f37fc2ed4a1022988f3
João Antônio Cardoso <joao.maker@gmail.com>
2025-09-26 13:29:47 -0300

    src: lib: stream: sink: Dont start sink before pipeline runner is set

--- src/lib/stream/sink/udp_sink.rs
+++ src/lib/stream/sink/udp_sink.rs
@@ -234,13 +234,6 @@ impl UdpSink {
         let pipeline_runner =
             PipelineRunner::try_new(&pipeline, &sink_id, true, video_and_stream_information)?;
 
-        // Start the pipeline
-        if let Err(state_err) = pipeline.set_state(gst::State::Playing) {
-            return Err(anyhow!(
-                "Failed starting UdpSink's pipeline: {state_err:#?}"
-            ));
-        }
-
         Ok(Self {
             sink_id: sink_id.clone(),
             pipeline,
