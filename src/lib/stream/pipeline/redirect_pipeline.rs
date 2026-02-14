99b59dba03c03a6f136c6c7fc6f509a642beb797
Patrick José Pereira <patrickelectric@gmail.com>
2026-02-13 21:25:16 -0300

    Move rtspsrc to use TCP over UDP
    Decrease the CPU usage
    
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/lib/stream/pipeline/redirect_pipeline.rs
+++ src/lib/stream/pipeline/redirect_pipeline.rs
@@ -65,7 +65,7 @@ impl RedirectPipeline {
             "rtsp" => {
                 format!(
                     concat!(
-                        "rtspsrc location={location} is-live=true latency=0 do-retransmission=true",
+                        "rtspsrc location={location} is-live=true latency=0 do-retransmission=true protocols=tcp",
                         " ! application/x-rtp, media=(string)video",
                     ),
                     location = url,
