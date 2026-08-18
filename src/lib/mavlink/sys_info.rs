150abfe0c8cee51525ec1ada0a85bd55dc20b7ea
João Antônio Cardoso <joao.maker@gmail.com>
2026-08-17 23:19:47 -0300

    src: lib: mavlink: sys_info: Read only the disks, not every process
    This drops from ~120ms ~34us per call.

--- src/lib/mavlink/sys_info.rs
+++ src/lib/mavlink/sys_info.rs
@@ -1,4 +1,4 @@
-use sysinfo::{DiskExt, System, SystemExt};
+use sysinfo::{DiskRefreshKind, Disks, System};
 use tracing::*;
 
 #[derive(Debug)]
@@ -15,11 +15,9 @@ pub fn sys_info() -> SysInfo {
     let mut local_total_capacity = 0;
     let mut local_available_capacity = 0;
 
-    let mut system = System::new_all();
-    system.refresh_disks();
+    let disks = Disks::new_with_refreshed_list_specifics(DiskRefreshKind::nothing().with_storage());
 
-    let main_disk = system
-        .disks()
+    let main_disk = disks
         .iter()
         .find(|disk| disk.mount_point().as_os_str() == "/");
     match main_disk {
@@ -33,7 +31,7 @@ pub fn sys_info() -> SysInfo {
         }
     }
 
-    let boottime_ms = system.boot_time() * 1000;
+    let boottime_ms = System::boot_time() * 1000;
 
     SysInfo {
         time_boot_ms: boottime_ms as u32,
